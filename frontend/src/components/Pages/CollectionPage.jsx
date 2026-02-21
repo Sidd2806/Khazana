42e50e87d0497d155c1a3ff14ea02a3ad7b2c304
Himanshu Maurya <mauryah376@email.com>
2026-02-21 20:18:36 +0530

    created filterSideBar

--- frontend/src/components/Pages/CollectionPage.jsx
+++ frontend/src/components/Pages/CollectionPage.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
-
+import React, { useEffect, useRef, useState } from "react";
+import { FaFilter } from "react-icons/fa";
+import FilterSideBar from "./FilterSideBar";
+import SortOptions from "./SortOptions";
+import ProductGrid from "../../components/Products/ProductGrid"
 const CollectionPage = () => {
-  return (
-    <div>CollectionPage</div>
-  )
-}
+  const [products, setProducts] = useState([]);
+  const sidebarRef = useRef(null);
+  const [isSeideBarOpen, setIsSeideBarOpen] = useState(false);
 
-export default CollectionPage
+  const toggleSideBar = () => {
+    setIsSeideBarOpen(!isSeideBarOpen);
+  };
+
+  const handleClickOutside = (e) => {
+    // 1 sideref.current check is sidebar exist or not
+    // 2 sideref.current.contains means the actual sidebar dom and e.target give the location of the user clciked and it beinged chekc wheter it is inside or outside
+    if (sidebarRef.current && !sidebarRef.current.contains(e.target)) {
+      setIsSeideBarOpen(false);
+    }
+  };
+  useEffect(() => {
+    //add event listneter so when you click outside the sidebar it will close
+    document.addEventListener("mousedown", handleClickOutside);
+    // remove addEventListener
+    document.removeEventListener("mousedown", handleClickOutside);
+  });
+  useEffect(() => {
+    setTimeout(() => {
+      const fetchedProducts = [
+        {
+          _id: 1,
+          name: "Product 1",
+          price: "100",
+          images: [{ url: "https://picsum.photos/500/500?random=7" }],
+        },
+        {
+          _id: 2,
+          name: "Product 2",
+          price: "200",
+          images: [{ url: "https://picsum.photos/500/500?random=8" }],
+        },
+        {
+          _id: 3,
+          name: "Product 3",
+          price: "300",
+          images: [{ url: "https://picsum.photos/500/500?random=9" }],
+        },
+        {
+          _id: 4,
+          name: "Product 4",
+          price: "500",
+          images: [{ url: "https://picsum.photos/500/500?random=10" }],
+        },
+        {
+          _id: 5,
+          name: "Product 5",
+          price: "100",
+          images: [{ url: "https://picsum.photos/500/500?random=11" }],
+        },
+        {
+          _id: 6,
+          name: "Product 6",
+          price: "200",
+          images: [{ url: "https://picsum.photos/500/500?random=12" }],
+        },
+        {
+          _id: 7,
+          name: "Product 7",
+          price: "300",
+          images: [{ url: "https://picsum.photos/500/500?random=13" }],
+        },
+        {
+          _id: 8,
+          name: "Product 8",
+          price: "500",
+          images: [{ url: "https://picsum.photos/500/500?random=14" }],
+        },
+      ];
+      setProducts(fetchedProducts);
+    }, 1000);
+  }, []);
+  return (
+    <div className="flex flex-col lg:flex-row">
+      {/* mobile */}
+      <button
+        onClick={toggleSideBar}
+        className="lg:hidden border p-2 flex justify-center items-center"
+      >
+        <FaFilter className="mr-2" />
+      </button>
+      {/* filter side bar */}
+      <div
+        ref={sidebarRef}
+        className={`${isSeideBarOpen ? "translate-x-0" : "-translate-x-full"}
+      fixed inset-0 z-50  bg-white w-64 overflow-y-auto transition-transform duration-300 lg:static lg:translate-x-0`}
+      >
+        <FilterSideBar />
+        </div>
+        <div className="grow p-4">
+          <div className="text-2xl uppercase mb-4">All collections</div>
+        {/* Sort options */}
+        <SortOptions />
+        <ProductGrid products={products} /> 
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPage;
---
+++ frontend/src/components/Pages/FilterSideBar.jsx
@@ -0,0 +1,214 @@
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const FilterSideBar = () => {
+  // read and update URL query parameters
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [Filters, setFilters] = useState({
+    category: "",
+    gender: "",
+    color: "",
+    size: [],
+    material: [],
+    brand: [],
+    minPrice: 0,
+    maxPrice: 100,
+  });
+  const [PriceRange, setPriceRange] = useState([0, 100]);
+  const categories = ["Top Wear", "Bottom Wear"];
+  const colors = [
+    "Red",
+    "Blue",
+    "Black",
+    "Green",
+    "Yellow",
+    "Gray",
+    "White",
+    "Pink",
+    "Beige",
+    "Navy",
+  ];
+  const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
+  const materials = [
+    "Cotton",
+    "Wool",
+    "Denim",
+    "Polyster",
+    "Silk",
+    "Linen",
+    "Viscose",
+    "Fleece",
+  ];
+  const brands = [
+    "Urban Threads",
+    "Modern Fit",
+    "Street Style",
+    "Beach Breeze",
+    "Fashionista",
+    "ChicStyle",
+  ];
+  const gender = ["Men", "Women"];
+  const handleFilterChange= (e)=>{
+    const {name,value,checked,type}=e.target
+    let newFilters= {...Filters}
+    if(type=="checkbox"){
+      if(checked){
+        newFilters[name]=[...(newFilters[name] || []),value]
+      }else {
+        newFilters[name]=newFilters[name].filter((item)=>item!==value)
+      }
+    }
+    else {
+      newFilters[name]=value;
+    }
+    console.log("hello",{name,value,checked,type});
+    
+  }
+  useEffect(() => {
+    const params = Object.fromEntries([...searchParams]); // convert all the params queries into json
+    setFilters({
+      category: params.category || "",
+      gender: params.gender || "",
+      color: params.color || "",
+      size: params.size ? params.size.split(",") : [],
+      material: params.material ? params.material.split(",") : [],
+      brand: params.brand ? params.brand.split(",") : [],
+      minPrice: params.minPrice || 0,
+      maxPrice: params.maxPrice || 100,
+    });
+    setPriceRange([0, params.maxPrice || 100]);
+  }, [searchParams]);
+  return (
+    <div className="p-4">
+      <h3 className="text-xl font-medium text-gray-800 mb-4 border-b">
+        Filter
+      </h3>
+
+      {/* Category Filters */}
+      <div className="mb-6 \">
+        <label className="block text-gray-700 mb-2 font-medium">Category</label>
+        {categories.map((category, index) => (
+          <div key={index} className="flex items-center mb-1">
+            <input
+              type="radio"
+              name="category"
+              value={category}
+              onChange={handleFilterChange}
+              checked={Filters.category === category} // this is for the page reload that the choice is remain intact
+              className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
+            />
+            <span className="text-gray-600">{category}</span>
+          </div>
+        ))}
+      </div>
+      {/* Gender filter */}
+      <div className="mb-6">
+        <label className="block text-gray-700 mb-2 font-medium">Gender</label>
+        {gender.map((gen, index) => (
+          <div key={index} className="flex items-center mb-1">
+            <input
+              type="radio"
+              name="gender"
+              value={gen}
+              onChange={handleFilterChange}
+              checked={Filters.gender === gen}
+              className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
+            />
+            <span className="text-gray-500">{gen}</span>
+          </div>
+        ))}
+      </div>
+      {/* color Filters we make this like a visible colors dikhna chahiye lets do it*/}
+      <div className="mb-6">
+        <label className="block text-gray-600 font-medium mb-2">Colors</label>
+        <div className="flex flex-wrap gap-3">
+          {colors.map((color) => (
+            <button
+              className={`rounded-full w-8 h-8 border border-gray-300 cursor-pointer transition hover:scale-125 ${Filters.color === color ? "ring-2 ring-blue-500" : ""}`}
+              key={color}
+              style={{ backgroundColor: color.toLowerCase() }}
+              type="button"
+              name="color"
+              value={color}
+              onClick={handleFilterChange}
+            ></button>
+          ))}
+        </div>
+      </div>
+      {/* size Filter is an array so we have to make it checkbox type */}
+      <div className="mb-6">
+        <label className="block text-gray-600 font-medium mb-2">Sizes</label>
+        {sizes.map((size, index) => (
+          <div key={index} className="flex items-center mb-1">
+            <input
+              type="checkbox"
+              name="size"
+              className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
+              value={size}
+              onChange={handleFilterChange}
+              checked={Filters.size.includes(size)}
+            />
+            <span className="text-gray-700">{size}</span>
+          </div>
+        ))}
+      </div>
+      {/*material filter*/}
+      <div className="mb-6">
+        <label className="block text-gray-600 font-medium mb-2">
+          Materials
+        </label>
+        {materials.map((material, index) => (
+          <div key={index} className="flex items-center mb-1">
+            <input
+              type="checkbox"
+              name="material"
+              value={material}
+              // onChange={handleFilterChange}
+              // checked={Filters.material.includes(material)}
+              className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
+            />
+            <span className="text-gray-700">{material}</span>
+          </div>
+        ))}
+      </div>
+      {/* brand filter*/}
+      <div className="mb-6">
+        <label className="block text-gray-600 font-medium mb-2">Brands</label>
+        {brands.map((brand, index) => (
+          <div key={index} className="flex items-center mb-1">
+            <input
+              type="checkbox"
+              name="brand"
+              value={brand}
+              onChange={handleFilterChange}
+              checked={Filters.brand.includes(brand)}
+              className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
+            />
+            <span className="text-gray-700">{brand}</span>
+          </div>
+        ))}
+      </div>
+      {/* price ramge */}
+      <div className="mb-8">
+        <label className="block text-gray-600 font-medium mb-2">
+          Price Range
+        </label>
+        <input
+          type="range"
+          name="priceRange"
+          min={0}
+          max={100}
+          // value={PriceRange[1]}
+          // onChange={handlePriceRange}
+          className="w-full h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer"
+        />
+        <div className="flex justify-between text-gray-600 mt-2">
+          <span>$0</span>
+          <span>${PriceRange[1]}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FilterSideBar;
