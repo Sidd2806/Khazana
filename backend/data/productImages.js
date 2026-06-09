56f50d2097d406594540313602df9b0d1cb14a3e
Himanshu Maurya <mauryah376@email.com>
2026-06-09 13:04:50 +0530

    Update product images to use Cloudinary URLs

--- backend/data/productImages.js
+++ backend/data/productImages.js
@@ -1,129 +1,129 @@
 const productImages = {
   "OX-SH-001": [
-    "/pictures/01-classic-oxford-button-down-shirt-1.jpg",
-    "/pictures/01-classic-oxford-button-down-shirt-2.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/01-classic-oxford-button-down-shirt-1.jpg",
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/01-classic-oxford-button-down-shirt-2.jpg"
   ],
   "SLIM-SH-002": [
-    "/pictures/02-slim-fit-stretch-shirt-1.jpg",
-    "/pictures/02-slim-fit-stretch-shirt-2.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/02-slim-fit-stretch-shirt-1.jpg",
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/02-slim-fit-stretch-shirt-2.jpg"
   ],
   "CAS-DEN-003": [
-    "/pictures/03-casual-denim-shirt-1.jpg",
-    "/pictures/03-casual-denim-shirt-2.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/03-casual-denim-shirt-1.jpg",
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/03-casual-denim-shirt-2.jpg"
   ],
   "PRNT-RES-004": [
-    "/pictures/04-printed-resort-shirt-1.jpg",
-    "/pictures/04-printed-resort-shirt-2.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/04-printed-resort-shirt-1.jpg",
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/04-printed-resort-shirt-2.jpg"
   ],
   "SLIM-EIR-005": [
-    "/pictures/05-slim-fit-easy-iron-shirt-1.jpg",
-    "/pictures/05-slim-fit-easy-iron-shirt-2.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/05-slim-fit-easy-iron-shirt-1.jpg",
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/05-slim-fit-easy-iron-shirt-2.jpg"
   ],
   "POLO-TSH-006": [
-    "/pictures/06-polo-t-shirt-with-ribbed-collar-1.jpg",
-    "/pictures/06-polo-t-shirt-with-ribbed-collar-2.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/06-polo-t-shirt-with-ribbed-collar-1.jpg",
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/06-polo-t-shirt-with-ribbed-collar-2.jpg"
   ],
   "OVS-GRF-007": [
-    "/pictures/07-oversized-graphic-t-shirt-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/07-oversized-graphic-t-shirt-1.jpg"
   ],
   "REG-HEN-008": [
-    "/pictures/08-regular-fit-henley-shirt-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/08-regular-fit-henley-shirt-1.jpg"
   ],
   "LST-THR-009": [
-    "/pictures/09-long-sleeve-thermal-tee-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/09-long-sleeve-thermal-tee-1.jpg"
   ],
   "VNECK-CLS-010": [
-    "/pictures/10-v-neck-classic-t-shirt-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/10-v-neck-classic-t-shirt-1.jpg"
   ],
   "BW-001": [
-    "/pictures/11-slim-fit-joggers-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/11-slim-fit-joggers-1.jpg"
   ],
   "BW-002": [
-    "/pictures/12-cargo-joggers-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/12-cargo-joggers-1.jpg"
   ],
   "BW-003": [
-    "/pictures/13-tapered-sweatpants-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/13-tapered-sweatpants-1.jpg"
   ],
   "BW-004": [
-    "/pictures/14-denim-jeans-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/14-denim-jeans-1.jpg"
   ],
   "BW-005": [
-    "/pictures/15-chino-pants-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/15-chino-pants-1.jpg"
   ],
   "BW-006": [
-    "/pictures/16-track-pants-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/16-track-pants-1.jpg"
   ],
   "BW-007": [
-    "/pictures/17-slim-fit-trousers-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/17-slim-fit-trousers-1.jpg"
   ],
   "BW-008": [
-    "/pictures/18-cargo-pants-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/18-cargo-pants-1.jpg"
   ],
   "BW-009": [
-    "/pictures/19-relaxed-fit-sweatpants-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/19-relaxed-fit-sweatpants-1.jpg"
   ],
   "BW-010": [
-    "/pictures/20-formal-dress-pants-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/20-formal-dress-pants-1.jpg"
   ],
   "BW-W-001": [
-    "/pictures/21-high-waist-skinny-jeans-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/21-high-waist-skinny-jeans-1.jpg"
   ],
   "BW-W-002": [
-    "/pictures/22-wide-leg-trousers-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/22-wide-leg-trousers-1.jpg"
   ],
   "BW-W-003": [
-    "/pictures/23-stretch-leggings-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/23-stretch-leggings-1.jpg"
   ],
   "BW-W-004": [
-    "/pictures/24-pleated-midi-skirt-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/24-pleated-midi-skirt-1.jpg"
   ],
   "BW-W-005": [
-    "/pictures/25-flared-palazzo-pants-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/25-flared-palazzo-pants-1.jpg"
   ],
   "BW-W-006": [
-    "/pictures/26-high-rise-joggers-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/26-high-rise-joggers-1.jpg"
   ],
   "BW-W-007": [
-    "/pictures/27-paperbag-waist-shorts-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/27-paperbag-waist-shorts-1.jpg"
   ],
   "BW-W-008": [
-    "/pictures/28-stretch-denim-shorts-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/28-stretch-denim-shorts-1.jpg"
   ],
   "BW-W-009": [
-    "/pictures/29-culottes-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/29-culottes-1.jpg"
   ],
   "BW-W-010": [
-    "/pictures/30-classic-pleated-trousers-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/30-classic-pleated-trousers-1.jpg"
   ],
   "TW-W-001": [
-    "/pictures/31-knitted-cropped-top-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/31-knitted-cropped-top-1.jpg"
   ],
   "TW-W-002": [
-    "/pictures/32-boho-floral-blouse-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/32-boho-floral-blouse-1.jpg"
   ],
   "TW-W-003": [
-    "/pictures/33-casual-t-shirt-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/33-casual-t-shirt-1.jpg"
   ],
   "TW-W-004": [
-    "/pictures/34-off-shoulder-top-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/34-off-shoulder-top-1.jpg"
   ],
   "TW-W-005": [
-    "/pictures/35-lace-trimmed-cami-top-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/35-lace-trimmed-cami-top-1.jpg"
   ],
   "TW-W-006": [
-    "/pictures/36-graphic-print-tee-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/36-graphic-print-tee-1.jpg"
   ],
   "TW-W-007": [
-    "/pictures/37-ribbed-long-sleeve-top-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/37-ribbed-long-sleeve-top-1.jpg"
   ],
   "TW-W-008": [
-    "/pictures/38-ruffle-sleeve-blouse-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/38-ruffle-sleeve-blouse-1.jpg"
   ],
   "TW-W-009": [
-    "/pictures/39-classic-button-up-shirt-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/39-classic-button-up-shirt-1.jpg"
   ],
   "TW-W-010": [
-    "/pictures/40-v-neck-wrap-top-1.jpg"
+    "https://res.cloudinary.com/dn0yjwlo4/image/upload/v1/khazana/products/40-v-neck-wrap-top-1.jpg"
   ]
 };
 
